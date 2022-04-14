8b8c08a16176c8fe8f155a3ba2011bab4e4f86e9
Edouard Roussel <edouardroussel@Edouards-MacBook-Pro.local>
2022-04-14 15:13:02 +0200

    bdd ready

--- routes/index.js
+++ routes/index.js
@@ -1,36 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-const mongoose = require('mongoose');
+var journeyModel = require('../models/journey')
 
-// useNewUrlParser ;)
-var options = {
-  connectTimeoutMS: 5000,
-  useNewUrlParser: true,
-  useUnifiedTopology: true
- };
-
-// --------------------- BDD ----------------------------------------------------
-mongoose.connect('mongodb+srv://XXXXXXXX:*********@XXXXXXXX-0hsfc.mongodb.net/Ticketac?retryWrites=true',
-   options,
-   function(err) {
-    if (err) {
-      console.log(`error, failed to connect to the database because --> ${err}`);
-    } else {
-      console.info('*** Database Ticketac connection : Success ***');
-    }
-   }
-);
-
-var journeySchema = mongoose.Schema({
-  departure: String,
-  arrival: String,
-  date: Date,
-  departureTime: String,
-  price: Number,
-});
-
-var journeyModel = mongoose.model('journey', journeySchema);
 
 var city = ["Paris","Marseille","Nantes","Lyon","Rennes","Melun","Bordeaux","Lille"]
 var date = ["2018-11-20","2018-11-21","2018-11-22","2018-11-23","2018-11-24"]
@@ -58,72 +30,15 @@ router.get('/homepage', function(req, res, next) {
   res.render('homepage', {});
 });
 
-router.get('/result', function(req, res, next) {
+router.get('/result',  function(req, res, next) {
+ 
   res.render('result', {});
 });
 
-router.get('/basket', function(req, res, next) {
+router.get('/basket', async function(req, res, next) {
+  var result = await journeyModel.find()
+  console.log(result);
   res.render('basket', {});
-});
-
-router.get('/save', function(req, res, next) {
-  res.render('', {});
-});
-
-// bdd = mongodb+srv://EdouardROUSSEL:3iE0DoUomLswhzgV@cluster0.uvhwb.mongodb.net/Ticktak?retryWrites=true&w=majority
-// 3iE0DoUomLswhzgV
-
-// Remplissage de la base de donnée, une fois suffit
-router.get('/save', async function(req, res, next) {
-
-  // How many journeys we want
-  var count = 300
-
-  // Save  ---------------------------------------------------
-    for(var i = 0; i< count; i++){
-
-    departureCity = city[Math.floor(Math.random() * Math.floor(city.length))]
-    arrivalCity = city[Math.floor(Math.random() * Math.floor(city.length))]
-
-    if(departureCity != arrivalCity){
-
-      var newUser = new journeyModel ({
-        departure: departureCity , 
-        arrival: arrivalCity, 
-        date: date[Math.floor(Math.random() * Math.floor(date.length))],
-        departureTime:Math.floor(Math.random() * Math.floor(23)) + ":00",
-        price: Math.floor(Math.random() * Math.floor(125)) + 25,
-      });
-       
-       await newUser.save();
-
-    }
-
-  }
-  res.render('index', { title: 'Express' });
-});
-
-
-// Cette route est juste une verification du Save.
-// Vous pouvez choisir de la garder ou la supprimer.
-router.get('/result', function(req, res, next) {
-
-  // Permet de savoir combien de trajets il y a par ville en base
-  for(i=0; i<city.length; i++){
-
-    journeyModel.find( 
-      { departure: city[i] } , //filtre
-  
-      function (err, journey) {
-
-          console.log(`Nombre de trajets au départ de ${journey[0].departure} : `, journey.length);
-      }
-    )
-
-  }
-
-
-  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
